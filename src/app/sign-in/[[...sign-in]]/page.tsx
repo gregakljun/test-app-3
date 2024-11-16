c86ef7914a4eb7da80a40806a5f2faccbfacb4be
gregakljun <138165368+gregakljun@users.noreply.github.com>
2024-11-16 20:26:29 +0100

    Update page.tsx

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,5 @@
 import { SignIn } from '@clerk/nextjs';
 
-export default function Page() {
-  return (
-    <div className='flex items-center justify-center flex-col gap-10'>
-      <h1 className='text-4xl font-bold mt-20'>This is signin page</h1>
-      <SignIn />
-    </div>
-  );
-}
 export async function generateStaticParams() {
   const posts = await fetch('https://.../posts').then((res) => res.json())
  
@@ -20,5 +12,10 @@ export async function generateStaticParams() {
 // using the `params` returned by `generateStaticParams`
 export default async function Page({ params }) {
   const { slug } = await params
-  // ...
+  return (
+      <div className='flex items-center justify-center flex-col gap-10'>
+        <h1 className='text-4xl font-bold mt-20'>This is signin page</h1>
+        <SignIn />
+      </div>
+    );
 }
